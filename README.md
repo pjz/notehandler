a18de696a50cfd987250f79e766a99b05c1b0b4a
Paul Jimenez <pj@place.org>
2014-01-15 10:11:10 -0500

    Working notebook and notebooks commands.

--- README.md
+++ README.md
@@ -8,9 +8,23 @@ An Evernote CLI in python.
 
 cn login/logout - acquire and destroy oauth tokens
 
-cn add [--tags|-t <taglist>]
-       [--notebook|-n <notebook>] 
-       [--embed
-       <title> <content>
+cn add [[:<tag1>] [:<tag2>] ...]
+       [+<notebook>] 
+       <title> 
+       [resource1] [resource2] ..
+       < <content>
     add a new note 
+       with the specified tags (or none if unspecified)
+       in the specified notebook (or your current notebook if unspecified)
+       with the specified title (required)
+       adding the specified files as resources to that note (or none if unspecified)
+        
+       
+cn notebooks - list existing notebooks. 
+                 Your default notebook is marked with a 'D'.
+                 Your (clevernote-only) current notebook is marked with a '+'
+
+cn notebook [+<notebook>] - if notebook is specified, set the current notebook,
+                               creating it if necessary. Otherwise, just show
+                               the current notebook.
 
--- clevernote.py
+++ clevernote.py
@@ -2,8 +2,10 @@ import sys
 import json
 import os.path
 import urllib2
+import functools
 
 from evernote.api.client import EvernoteClient
+import evernote.edam.type.ttypes as Types
 
 CLEVERNOTE_CONSUMER_KEY = ''
 CLEVERNOTE_CONSUMER_SECRET = ''
@@ -34,6 +36,18 @@ def save_config():
              , indent=4
              , separators=(',', ': ')
              )
+
+
+def memoize(obj):
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
 
 
 def cmd_login(args):
@@ -74,6 +88,7 @@ def cmd_logout(args):
     print("Logged out.")
 
 
+@memoize
 def get_client():
     load_config()
     if 'dev_token' in config:
@@ -86,16 +101,67 @@ def get_client():
     return EvernoteClient(token=access_token)
 
 
-def cmd_notebooks(args):
-    """notebooks - Show the list of existing notebooks"""
+@memoize
+def get_notebooks():
+    return get_client().get_note_store().listNotebooks()
 
-    client = get_client()
-    noteStore = client.get_note_store()
-    notebooks = noteStore.listNotebooks()
+
+def get_current_notebook():
+    load_config()
+    if 'cur_notebook' not in config:
+        cur = [ n.name for n in get_notebooks() if n.defaultNotebook ]
+        config['cur_notebook'] = cur[0]
+        save_config()
+    return config['cur_notebook']
+
+
+def ask_yn(prompt):
+    result = raw_input(prompt)
+    return result.lower() in [ 'y', 'yes' ]
+
+
+def set_current_notebook(name):
+    load_config()
+    if name not in [ n.name for n in get_notebooks() ]:
+        if not ask_yn("Notebook '%s' doesn't exist. Create it?" % name):
+            print("Aborted.")
+            sys.exit(1)
+        notebook = Types.Notebook()
+        notebook.name = name
+        note_store = get_client().get_note_store()
+        notebook = note_store.createNotebook(notebook)
+    config['cur_notebook'] = name
+    save_config()
+
+
+def cmd_notebooks(args):
+    """notebooks - List existing notebooks. 
+    The (evernote) default notebook is marked with a 'D'.
+    The (local-only) current notebook is marked with a '+'
+    """
 
     print("Notebooks:")
-    for n in notebooks:
-        print(n.name)
+    for n in get_notebooks():
+        print("  %s%s%s" % ( 'D' if n.defaultNotebook else ' '
+                           , '+' if n.name == get_current_notebook() else ' '
+                           , n.name))
+
+
+def cmd_notebook(args):
+    """notebook [+<notebook>] - if notebook is specified, set the current notebook,
+    creating it if necessary. Otherwise, just show the current notebook.
+    """
+     
+    if len(args) > 0:
+        name = args[0]
+        if name.startswith('+'): name = name[1:]
+        set_current_notebook(name)
+
+    print("Current notebook set to '%s'." % get_current_notebook())
+
+
+
+
 
 
 def cmd_userinfo(args):
