e56e2d05dd027368565332420b86ed7ce8018b98
Paul Jimenez <pj@place.org>
2014-09-02 15:39:24 -0400

    Release 0.6

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup( author = 'Paul Jimenez'
      , py_modules = [ 'distribute_setup', 'notehandler' ]
      , url = 'http://github.com/pjz/notehandler'
      # there must be nothing on the following line after the = other than a string constant
-     , version = '0.5-dev'
+     , version = '0.6'
      , zip_safe = False
      , install_requires = [ 'evernote', 'cmdpy' ]
       )
